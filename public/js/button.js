d089a4c26816abee11d924ae0d6d9531240c064c
Ju Jinsol <twoxo@naver.com>
2024-12-05 14:37:58 +0900

    button, test 삭제

--- public/js/button.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('moveButton').addEventListener('click', function () {
-    var moveModalContainer = document.querySelector('.moveModalContainer');
-    moveModalContainer.classList.add('active'); // 모달을 보이도록 active 클래스 추가
-});
-
-document.getElementById('myChatBox').innerHTML = (
-    "hi"
-);
